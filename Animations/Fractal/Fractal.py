d246ac925b65f3018591f68de297bd238763d783
Benjamin Le Grand <38102972+Benjamin-LeGrand@users.noreply.github.com>
2018-11-07 18:57:54 +0100

    Update Fractal.py

--- Animations/Fractal/Fractal.py
+++ Animations/Fractal/Fractal.py
@@ -19,36 +19,35 @@ class Matrice:
 		self.tabCouche=[]
 #class Matrice
 
-def init_lignes(couche):
-	nbLigne=int((((len(couche.tabCarre))*3)/(math.sqrt(len(couche.tabCarre)))))
+def init_lignes(Couche):
+	nbLigne=int((((len(Couche.tabCarre))*3)/(math.sqrt(len(Couche.tabCarre)))))
 
 	for i in range(0,nbLigne):
 		tmp=[]
-		couche.Lignes.append(tmp)
-	#on organise la couche en ligne
+		Couche.Lignes.append(tmp)
 
-	for carre in couche.tabCarre:
+	for carre in Couche.tabCarre:
 		compteur = int(0)
 		for elem in carre.tabVal:
-			numLigne=int((couche.tabCarre.index(carre) // math.sqrt(len(couche.tabCarre)) * 3) + ((compteur) // ( math.sqrt(len(carre.tabVal)))))
-			couche.Lignes[numLigne].append(elem)
+			numLigne=int((Couche.tabCarre.index(carre) // math.sqrt(len(Couche.tabCarre)) * 3) + ((compteur) // ( math.sqrt(len(carre.tabVal)))))
+			Couche.Lignes[numLigne].append(elem)
 			compteur = compteur +1
-	#print('lignes de la couche initialisees')
-	return couche
+	return Couche
 
-def affiche_lignes(couche):
-	for ligne in couche.Lignes:  
+def affiche_lignes(Couche):
+	for ligne in Couche.Lignes:  
 		for elem in ligne:
-			print(elem, end=' ') 
+			print(elem,end=' ') 
 		print()
+	print()
 	#affiche matrice
 
-def init_affiche_ligne(couche):
-	init_lignes(couche)
-	affiche_lignes(couche)
+def init_affiche_ligne(Couche):
+	init_lignes(Couche)
+	affiche_lignes(Couche)
 
-def affiche_mat(matrice):
-	for couches in matrice.tabCouche:
+def affiche_mat(Matrice):
+	for couches in Matrice.tabCouche:
 		init_affiche_ligne(couches)
 
 def initialize():
@@ -61,15 +60,10 @@ def initialize():
 	return basicMatrice
 
 def upgrade_matrice(oldMatrice,newMatrice):
-	#print('etape de lancienne matrice : ' , oldMatrice.etape)
 	newMatrice.etape=oldMatrice.etape + 1
-	#print('etape de la nouvelle matrice : ' , newMatrice.etape)
 	nbCouche = int((3**(newMatrice.etape)-1)/2)
-	#print('nombre de couche : ',nbCouche)
 	nbCarre = int((3**((newMatrice.etape)-1))**2)
-	#print('nombre de carre : ',nbCarre)
 	for couches in range (0,nbCouche):
-		#print('couche actuelle : ',couches)
 		newMatrice.tabCouche.append(Couche())
 		for carre in range (0,nbCarre):
 			newMatrice.tabCouche[couches].tabCarre.append(Carre())
@@ -88,6 +82,7 @@ def upgrade_matrice(oldMatrice,newMatrice):
 			else:
 				newMatrice.tabCouche[couches].tabCarre[carre].tabVal = [0,0,0,0,0,0,0,0,0]
 
+
 	for couches in range (0,nbCouche):
 		for carre in range (0,nbCarre):
 			if newMatrice.tabCouche[couches].tabCarre[carre].tabVal==[1,1,1,1,1,1,1,1,1] and couches%3==0:
@@ -98,38 +93,34 @@ def upgrade_matrice(oldMatrice,newMatrice):
 						newMatrice.tabCouche[couches].tabCarre[carre-1].tabVal[4]=1
 					if carre + 1 < nbCarre and carre%math.sqrt(nbCarre) != math.sqrt(nbCarre)-1:
 						newMatrice.tabCouche[couches].tabCarre[carre+1].tabVal[4]=1
-					#print(couches)
-					#print('carre ? : ' , carre)
-					#print('carre max auquel on veut acceder : ', int(carre + math.sqrt(nbCarre) - 1) )
 					if carre - math.sqrt(nbCarre) >= 0 :
 						newMatrice.tabCouche[couches].tabCarre[int(carre - math.sqrt(nbCarre))].tabVal[4]=1
-				   	if carre + math.sqrt(nbCarre) < nbCarre:
+					if carre + math.sqrt(nbCarre) < nbCarre:
 						newMatrice.tabCouche[couches].tabCarre[int(carre + math.sqrt(nbCarre))].tabVal[4]=1
 					if carre + math.sqrt(nbCarre) +1 < nbCarre and carre%math.sqrt(nbCarre) != math.sqrt(nbCarre)-1:
 						newMatrice.tabCouche[couches].tabCarre[int(carre + math.sqrt(nbCarre) + 1)].tabVal[4]=1
-				   	if  carre + math.sqrt(nbCarre) - 1 < nbCarre and carre%math.sqrt(nbCarre) != 0:
+					if  carre + math.sqrt(nbCarre) - 1 < nbCarre and carre%math.sqrt(nbCarre) != 0:
 						newMatrice.tabCouche[couches].tabCarre[int(carre + math.sqrt(nbCarre) - 1)].tabVal[4]=1
-				    	if carre - math.sqrt(nbCarre) -1 >= 0 and carre%math.sqrt(nbCarre) != 0:
+					if carre - math.sqrt(nbCarre) -1 >= 0 and carre%math.sqrt(nbCarre) != 0:
 						newMatrice.tabCouche[couches].tabCarre[int(carre - math.sqrt(nbCarre) - 1)].tabVal[4]=1
-				    	if carre - math.sqrt(nbCarre) + 1 >=0 and carre%math.sqrt(nbCarre) != math.sqrt(nbCarre)-1:
-                        			newMatrice.tabCouche[couches].tabCarre[int(carre - math.sqrt(nbCarre) + 1)].tabVal[4]=1
-				if newMatrice.tabCouche[couches].tabCarre[carre].tabVal==[1,1,1,1,1,1,1,1,1] and couches%3==1:
-					if carre - math.sqrt(nbCarre) >= 0:
-                    				newMatrice.tabCouche[couches].tabCarre[int(carre - math.sqrt(nbCarre))].tabVal[7]=1
-                			if carre + math.sqrt(nbCarre) < nbCarre:
-                    				newMatrice.tabCouche[couches].tabCarre[int(carre + math.sqrt(nbCarre))].tabVal[1]=1
-                			if carre - 1 >= 0 and carre%math.sqrt(nbCarre) != 0:
-                    				newMatrice.tabCouche[couches].tabCarre[int(carre - 1)].tabVal[5]=1
-                			if carre + 1 < nbCarre and carre%math.sqrt(nbCarre) != math.sqrt(nbCarre)-1:
-                    				newMatrice.tabCouche[couches].tabCarre[int(carre + 1)].tabVal[3]=1
+					if carre - math.sqrt(nbCarre) + 1 >=0 and carre%math.sqrt(nbCarre) != math.sqrt(nbCarre)-1:
+						newMatrice.tabCouche[couches].tabCarre[int(carre - math.sqrt(nbCarre) + 1)].tabVal[4]=1
+			if newMatrice.tabCouche[couches].tabCarre[carre].tabVal==[1,1,1,1,1,1,1,1,1] and couches%3==1:
+				if carre - math.sqrt(nbCarre) >= 0:
+					newMatrice.tabCouche[couches].tabCarre[int(carre - math.sqrt(nbCarre))].tabVal[7]=1
+				if carre + math.sqrt(nbCarre) < nbCarre:
+					newMatrice.tabCouche[couches].tabCarre[int(carre + math.sqrt(nbCarre))].tabVal[1]=1
+				if carre - 1 >= 0 and carre%math.sqrt(nbCarre) != 0:
+					newMatrice.tabCouche[couches].tabCarre[int(carre - 1)].tabVal[5]=1
+				if carre + 1 < nbCarre and carre%math.sqrt(nbCarre) != math.sqrt(nbCarre)-1:
+					newMatrice.tabCouche[couches].tabCarre[int(carre + 1)].tabVal[3]=1
 
 	return newMatrice
 
 def CreateMat(etape):
 	newMat=initialize()
-
-	for i in range(0, etape-1):
+	for i in range(0,etape-1):
 		nullMat = Matrice()
-		newMat = upgrade_matrice(newMat, nullMat)
-	
+		newMat = upgrade_matrice(newMat,nullMat)
+
 	return newMat
